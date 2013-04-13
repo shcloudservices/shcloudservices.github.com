a163b949c738b33aa64030e21775a2f389f13a6e
Adrian Moya <adrianmoya@gmail.com>
2013-04-13 10:27:56 -0500

    Site updated at 2013-04-13 15:27:56 UTC

---
+++ js/piwikbase.js
@@ -0,0 +1,2 @@
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.solucioneshospedadas.com/" : "http://piwik.solucioneshospedadas.com/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
